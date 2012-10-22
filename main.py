8a47e437b0fe2fcaf776571ae28a746491db2e45
YuviPanda <yuvipanda@gmail.com>
2012-10-22 05:58:52 +0530

    Added smooth paddle movement support
    Also does variable speed!

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProper
 from kivy.vector import Vector
 from kivy.factory import Factory
 from kivy.clock import Clock
+from kivy.logger import Logger
 
+import math
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
+    velocity = NumericProperty(0)
+    target_pos = NumericProperty(0)
+
+    speed = NumericProperty(0)
 
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
@@ -21,6 +27,22 @@ class PongPaddle(Widget):
             vel = bounced * 1.1
             ball.velocity = vel.x + curve, vel.y + offset
 
+    def move(self):
+        if self.center_y == self.target_pos:
+            self.target_pos = -1
+        if self.target_pos == -1 or self.speed == 0:
+            return
+        self.velocity = (self.target_pos - self.center_y)  / (4 / self.speed)
+        self.pos = Vector(0, self.velocity) + self.pos
+
+    def handle_touch(self, touch):
+        if touch.y == self.center_y:
+            self.target_pos = -1
+        else:
+            target_pos = touch.y
+            self.target_pos = target_pos
+            self.speed = abs(self.center_y - target_pos) / self.get_root_window().height
+            Logger.debug("Speed: %s" % self.speed)
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
@@ -42,6 +64,8 @@ class PongGame(Widget):
 
     def update(self, *args):
         self.ball.move()
+        self.player1.move()
+        self.player2.move()
 
         #bounce of paddles
         self.player1.bounce_ball(self.ball)
@@ -60,11 +84,20 @@ class PongGame(Widget):
             self.serve_ball(vel=(-4,0))
 
 
+    def on_touch_down(self, touch):
+        self.on_touch_move(touch)
+
     def on_touch_move(self, touch):
-        if touch.x < self.width/3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width/3:
-            self.player2.center_y = touch.y
+        if touch.x < self.width/2:
+            self.player1.handle_touch(touch)
+        if touch.x > self.width/2:
+            self.player2.handle_touch(touch)
+
+    def on_touch_up(self, touch):
+        if touch.x < self.width / 2:
+            self.player1.velocity = 0
+        if touch.x > self.width  / 2:
+            self.player2.velocity = 0
 
 
 Factory.register("PongBall", PongBall)
